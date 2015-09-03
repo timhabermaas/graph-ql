a29851160f066e021e6c59e2b13ee6c7f9a1995e
Tim Habermaas <tim.habermaas@gmail.com>
2015-09-04 01:02:31 +0200

    Add a bunch of code. Variables, Types and stuff.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Lib
     ( parseGraphQL
+    -- TODO move these to some internal type module which can be used from the tests
     , GQLDocument (..)
+    , GQLOperationDefinition (..)
     , GQLSelection (..)
+    , GQLVariable (..)
+    , GQLType (..)
+    , GQLBaseType (..)
     ) where
 
 import Data.Text
@@ -11,18 +16,28 @@ import Text.Parsec
 
 type GraphQLParser a = Parsec Text () a
 
-type Identifier = String
-data GQLSelection = GQLSelection Identifier deriving (Eq, Show)
+type Name = String
+data GQLSelection = GQLField Name GQLSelectionList deriving (Eq, Show)--GQLSelection Name GQLSelectionList deriving (Eq, Show)
 type GQLSelectionList = [GQLSelection]
-data GQLDocument = GQLQuery GQLSelectionList | GQLCommand | Nope deriving (Eq, Show)
+data GQLBaseType = GQLNamedType String | GQLListType String deriving (Eq, Show)
+data GQLType = GQLType GQLBaseType | GQLNullableType GQLBaseType deriving (Eq, Show)
+-- TODO int, float and stuff...
+type GQLDefaultValue = String
+data GQLVariable = GQLVariable Name GQLType (Maybe GQLDefaultValue) deriving (Eq, Show)
+-- FIXME This allows a query to have variables without being named which
+--       is actually not allowed by the specification.
+data GQLOperationDefinition = GQLQuery (Maybe Name) [GQLVariable] GQLSelectionList | GQLCommand | Nope deriving (Eq, Show)
+type GQLDocument = GQLOperationDefinition
 
 ignoredChars =
     skipMany ignoredChar
   where
+    -- TODO there are actually more characters to ignore
+    --      according to the spec
     ignoredChar = space <|> endOfLine
 
-identifier :: GraphQLParser Identifier
-identifier = do
+name :: GraphQLParser Name
+name = do
     c <- firstCharacter
     cs <- remainingCharacters
     return (c:cs)
@@ -33,24 +48,83 @@ identifier = do
 field :: GraphQLParser GQLSelection
 field = do
     ignoredChars
-    name <- identifier
+    name <- name
     ignoredChars
-    return $ GQLSelection name
+    selectionList <- (try selectionSetParser <|> return [])
+    ignoredChars
+    return $ GQLField name selectionList
 
-documentParser :: Parsec Text () GQLDocument
-documentParser = do
+selectionSetParser :: Parsec Text () GQLSelectionList
+selectionSetParser = do
+    ignoredChars
     char '{'
     ignoredChars
     fields <- many1 field
     ignoredChars
     char '}'
-    return $ GQLQuery fields
+    ignoredChars
+    return fields
+
+variableDefinitionParser :: Parsec Text () [GQLVariable]
+variableDefinitionParser = do
+    ignoredChars
+    char '('
+    ignoredChars
+    variables <- many1 variableParser
+    ignoredChars
+    char ')'
+    ignoredChars
+    return variables
+  where
+    variableParser = do
+      ignoredChars
+      char '$'
+      variableName <- name
+      ignoredChars
+      char ':'
+      ignoredChars
+      type' <- typeParser
+      ignoredChars
+      -- TODO: default value
+      return $ GQLVariable variableName type' Nothing
+    typeParser = do
+      type' <- listType <|> namedType
+      ignoredChars
+      -- TODO: Too cryptic?
+      try (GQLNullableType type' <$ char '!') <|> (return $ GQLType type')
+    namedType = do
+      name <- name
+      return $ GQLNamedType name
+    listType = do
+      char '['
+      ignoredChars
+      name <- name
+      ignoredChars
+      char ']'
+      return $ GQLListType name
 
 
-parseGraphQL :: Text -> Maybe GQLDocument
+operationDefinitionParser :: Parsec Text () GQLDocument
+operationDefinitionParser = do
+    ignoredChars
+    try shortSyntax <|> longSyntax
+  where
+    shortSyntax = do
+      fields <- selectionSetParser
+      return $ GQLQuery Nothing [] fields
+    longSyntax = do
+      string "query"
+      ignoredChars
+      name <- name
+      ignoredChars
+      variables <- try variableDefinitionParser <|> return []
+      ignoredChars
+      fields <- selectionSetParser
+      return $ GQLQuery (Just name) variables fields
+
+
+parseGraphQL :: Text -> Either ParseError GQLDocument
 parseGraphQL text =
-    case myParse documentParser text of
-        Right r -> Just r
-        Left _ -> Nothing
+    myParse operationDefinitionParser text
   where
     myParse rule text = parse rule "" text
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,8 +4,54 @@ import Test.Hspec
 import Lib
 import Data.Maybe (fromJust)
 
+p s =
+    case (parseGraphQL s) of
+        Right r -> r
+        Left l -> error $ show l
+
+q :: String -> [String] -> GQLDocument
+q name fs = GQLQuery (Just name) [] $ fmap (\field -> GQLField field []) fs
+
+t :: String -> GQLType
+t = GQLType . GQLNamedType
+
 main :: IO ()
 main = hspec $ do
   describe "parsing valid queries" $ do
-    it "can parse a query with two fields" $ do
-      (fromJust $ parseGraphQL "{ __firstName\nlastName }") `shouldBe` GQLQuery [GQLSelection "__firstName", GQLSelection "lastName"]
+    describe "shorthand syntax" $ do
+      it "simple" $ do
+        (p "{ name }") `shouldBe`
+         GQLQuery Nothing [] [GQLField "name" []]
+
+    describe "simple queries" $ do
+      it "with one fields" $ do
+        (p "query foo { abc_  }") `shouldBe`
+         q "foo" ["abc_"]
+      it "with two fields" $ do
+        (p "query foo { __firstName\nlastName }") `shouldBe`
+         q "foo" ["__firstName", "lastName"]
+
+    describe "queries with variables" $ do
+      it "one variable" $ do
+        (p "query foo($bar : String) { abc_  }") `shouldBe`
+         GQLQuery (Just "foo") [GQLVariable "bar" (t "String") Nothing] [GQLField "abc_" []]
+      it "two variables" $ do
+        (p "query foo($bar : String $foo: Integer) { abc_  }") `shouldBe`
+         GQLQuery (Just "foo") [GQLVariable "bar" (t "String") Nothing, GQLVariable "foo" (t "Integer") Nothing] [GQLField "abc_" []]
+      it "list types" $ do
+        (p "query foo($bar : [String]) { abc_  }") `shouldBe`
+         GQLQuery (Just "foo") [GQLVariable "bar" (GQLType (GQLListType "String")) Nothing] [GQLField "abc_" []]
+      it "nullable named types" $ do
+        (p "query foo($bar : [String] !) { abc_  }") `shouldBe`
+         GQLQuery (Just "foo") [GQLVariable "bar" (GQLNullableType (GQLListType "String")) Nothing] [GQLField "abc_" []]
+      it "nullable list types" $ do
+        (p "query foo($bar : [String] !) { abc_  }") `shouldBe`
+         GQLQuery (Just "foo") [GQLVariable "bar" (GQLNullableType (GQLListType "String")) Nothing] [GQLField "abc_" []]
+
+    describe "nested queries" $ do
+      it "one level deep" $ do
+        (p "query foo { parent { child } }") `shouldBe`
+         GQLQuery (Just "foo") [] [GQLField "parent" [GQLField "child" []]]
+      it "two levels deep" $ do
+        (p "query foo { parent { child { anotherCild }} }") `shouldBe`
+         GQLQuery (Just "foo") [] [GQLField "parent" [GQLField "child" [GQLField "anotherCild" []]]]
